7d062e4a9f30b1594ea22aaba36d8504b17ffcaf
jsonShadower999 <142488144+jsonShadower999@users.noreply.github.com>
2025-05-26 16:02:09 +0530

    Update Hashset.java

---
+++ hashmap/Hashset.java
@@ -0,0 +1,13 @@
+//  HashSet checks whether there is an existing entry using hashCode() and equals() methods.
+//  that why 
+//  public class Hashset<> extends Clonable , Serializable 
+//  default capacity is 16
+
+//method
+add
+clear 
+contains 
+remove 
+isEmpty 
+size 
+clone 
